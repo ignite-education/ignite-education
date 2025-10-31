2638d0ffa02ac09ae09bc87bce39fe6dcf1aadb4
ignite-education <hello@ignite.education>
2025-10-31 11:35:03 +0000

    Update ProgressHub.jsx

--- src/components/ProgressHub.jsx
+++ src/components/ProgressHub.jsx
@@ -2022,7 +2022,7 @@ const ProgressHub = () => {
                       style={{
                         right: '16px',
                         top: '50%',
-                        transform: 'translateY(-20%)',
+                        transform: 'translateY(-50%)',
                         width: '40px',
                         height: '40px',
                         borderRadius: '50%',
