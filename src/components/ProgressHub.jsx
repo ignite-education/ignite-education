64fe1360a1a0ce5a656a3f9f0a3dce7e833c4b25
ignite-education <hello@ignite.education>
2025-10-31 17:01:29 +0000

    Update ProgressHub.jsx

--- src/components/ProgressHub.jsx
+++ src/components/ProgressHub.jsx
@@ -2115,7 +2115,7 @@ const ProgressHub = () => {
               </div>
 
               {/* Merchandise */}
-              <div className="flex-shrink-0" style={{ marginTop: '16px', minHeight: '160px' }}>
+              <div className="flex-shrink-0" style={{ marginTop: '13.6px', minHeight: '160px' }}>
                 <h2 className="font-semibold" style={{ fontSize: '19px', marginBottom: '-2px' }}>Merchandise</h2>
                 <p className="text-white font-light" style={{ letterSpacing: '0.011em', fontSize: '14px', marginBottom: '2px' }}>All profit supports education projects across the UK.</p>
                 <div className="bg-white rounded-lg flex justify-between items-center gap-2" style={{ padding: '10px', paddingLeft: '14px', paddingRight: '14px', height: '100px' }}>
