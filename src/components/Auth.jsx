cb092df52ebad141742f5e3f0aa2462b42e87e80
ignite-education <hello@ignite.education>
2025-11-07 19:22:29 +0000

    Update Auth.jsx

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1891,26 +1891,6 @@ const Auth = () => {
                   </div>
                 )}
 
-                <div className="text-black mb-6 leading-relaxed" style={{ maxWidth: '90%' }}>
-                  {(() => {
-                    const description = selectedCourse.description || '';
-                    // Split by first period followed by space to get first sentence
-                    const firstSentenceEnd = description.indexOf('. ');
-                    if (firstSentenceEnd !== -1) {
-                      const firstSentence = description.substring(0, firstSentenceEnd + 1);
-                      const restOfText = description.substring(firstSentenceEnd + 2);
-                      return (
-                        <>
-                          <span style={{ fontWeight: 600, fontSize: '17px' }}>{firstSentence}</span>
-                          <br />
-                          <div style={{ marginTop: '0.6rem' }}>{restOfText}</div>
-                        </>
-                      );
-                    }
-                    return description;
-                  })()}
-                </div>
-
                 {/* Module and Lesson Details - Swipable Cards */}
                 {selectedCourse.module_structure && Array.isArray(selectedCourse.module_structure) && selectedCourse.module_structure.length > 0 ? (
                   <div className="mb-6 relative">
@@ -2077,6 +2057,26 @@ const Auth = () => {
                     </div>
                   </div>
                 )}
+
+                <div className="text-black mb-6 leading-relaxed" style={{ maxWidth: '90%' }}>
+                  {(() => {
+                    const description = selectedCourse.description || '';
+                    // Split by first period followed by space to get first sentence
+                    const firstSentenceEnd = description.indexOf('. ');
+                    if (firstSentenceEnd !== -1) {
+                      const firstSentence = description.substring(0, firstSentenceEnd + 1);
+                      const restOfText = description.substring(firstSentenceEnd + 2);
+                      return (
+                        <>
+                          <span style={{ fontWeight: 600, fontSize: '17px' }}>{firstSentence}</span>
+                          <br />
+                          <div style={{ marginTop: '0.6rem' }}>{restOfText}</div>
+                        </>
+                      );
+                    }
+                    return description;
+                  })()}
+                </div>
 
                 {/* Course Coaches Section */}
                 {courseCoaches[selectedCourseModal] && courseCoaches[selectedCourseModal].length > 0 && (
