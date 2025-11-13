b938bc39e4f282d99d6a4d287c079c761bf44730
ignite-education <hello@ignite.education>
2025-11-13 09:05:59 +0000

    clean up

--- verify-clean-database.sql
+++
@@ -1,4 +0,0 @@
--- Verify that the database is now clean
-SELECT COUNT(*) as total_lessons FROM lessons WHERE course_id = 'product-management';
-
--- This should return 0, meaning the database is clean and ready for new content
