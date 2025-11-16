0b8aacbe54d3140f7cc78b6495def3c04dc373d4
ignite-education <hello@ignite.education>
2025-11-16 17:18:55 +0000

    Update ProgressHub.jsx

--- src/components/ProgressHub.jsx
+++ src/components/ProgressHub.jsx
@@ -3553,7 +3553,7 @@ const ProgressHub = () => {
               {/* Left side - Features section (fixed) */}
               <div style={{ width: '45.6%', borderRadius: '0.3rem 0 0 0.3rem' }} className="bg-black p-8 flex flex-col justify-center">
                 <div style={{ marginTop: '-50px' }}>
-                <h3 className="text-white text-2xl font-medium mb-8" style={{ animation: 'fadeInUp 1.5s ease-out', animationDelay: '0.5s', opacity: 0, animationFillMode: 'forwards', lineHeight: '1.2' }}>
+                <h3 className="text-white text-2xl font-medium mb-8" style={{ animation: 'fadeInUp 1.5s ease-out', animationDelay: '0.5s', opacity: 0, animationFillMode: 'forwards', lineHeight: '1' }}>
                   <span className="font-light text-lg">For just 99p/week,</span><br />
                   <span className="text-pink-500" style={{ fontSize: '1.6rem' }}>get exclusive access to</span>
                 </h3>
