6426fcc84d25f53b42cc2bda3a38c8c57ee9c6c7
ignite-education <hello@ignite.education>
2025-11-30 19:00:28 +0000

    mobile

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -2737,7 +2737,7 @@ const Auth = () => {
                           <path strokeLinecap="round" strokeLinejoin="round" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
                         </svg>
                       </div>
-                      <span className="leading-tight">Taught by industry<br/>experts</span>
+                      <span className="leading-tight">Taught by<span className="hidden md:inline"><br/></span><span className="md:hidden"> </span>experts</span>
                     </div>
                     <div className="flex items-center" style={{ paddingLeft: '1rem' }}>
                       <div className="bg-white rounded p-0.5 flex-shrink-0" style={{ marginRight: '11.52px', transform: 'scale(1.92)' }}>
