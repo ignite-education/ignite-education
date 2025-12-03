e2778761648a187ca1045947be525afcd412692f
ignite-education <hello@ignite.education>
2025-12-03 07:54:16 +0000

    Update Auth.jsx

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1347,7 +1347,7 @@ const Auth = () => {
           animation: 'fadeIn 0.2s ease-out',
           zIndex: 50,
           scrollBehavior: 'smooth',
-          scrollSnapType: 'y mandatory',
+          scrollSnapType: isMobile ? 'none' : 'y mandatory',
           overflow: selectedCourseModal ? 'hidden' : 'auto',
           pointerEvents: selectedCourseModal ? 'none' : 'auto',
           minWidth: '1300px',
