e5718b422042450bbd1ee740514e813d15737ae0
ignite-education <hello@ignite.education>
2025-12-12 19:18:58 +0000

    Update Auth.jsx

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -2001,7 +2001,7 @@ const Auth = () => {
                     </p>
                   </div>
                   <img
-                    src="https://auth.ignite.education/storage/v1/object/public/assets/AI%20-%20v2.png"
+                    src="https://auth.ignite.education/storage/v1/object/public/assets/AI%20v5.png"
                     alt="Levelling up learning with smart AI integration"
                     style={{
                       width: isMobile ? '75vw' : '100%',
@@ -2031,7 +2031,7 @@ const Auth = () => {
                     </p>
                   </div>
                   <img
-                    src="https://auth.ignite.education/storage/v1/object/public/assets/Expert%20v3.png"
+                    src="https://auth.ignite.education/storage/v1/object/public/assets/Expert%20v5.png"
                     alt="Personalised support from industry professionals"
                     style={{
                       width: isMobile ? '75vw' : '100%',
@@ -2061,7 +2061,7 @@ const Auth = () => {
                     </p>
                   </div>
                   <img
-                    src="https://auth.ignite.education/storage/v1/object/public/assets/Community%20Forum%20v3.png"
+                    src="https://auth.ignite.education/storage/v1/object/public/assets/Community%20v5.png"
                     alt="Connect with the global community"
                     style={{
                       width: isMobile ? '75vw' : '100%',
