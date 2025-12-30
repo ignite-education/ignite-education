8c7921e46c985f386504974a33283977be58cd2e
ignite-education <hello@ignite.education>
2025-12-30 17:36:14 +0000

    Update Auth.jsx

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -2260,8 +2260,8 @@ const Auth = () => {
                   </div>
                   
                   {/* Carousel Indicators */}
-                  <div style={{ width: '36rem', marginTop: '1rem', marginLeft: '1.25rem', display: 'flex', justifyContent: isMobile ? 'flex-start' : 'center' }}>
-                    <div className="auth-testimonial-indicators flex justify-start gap-2" style={{ width: isMobile ? 'auto' : '80%' }}>
+                  <div style={{ width: '36rem', marginTop: '1rem', marginLeft: '1.25rem', padding: '0 1.5rem', display: 'flex', justifyContent: 'center', boxSizing: 'border-box' }}>
+                    <div className="auth-testimonial-indicators flex justify-start gap-2" style={{ width: '80%' }}>
                     {[0, 1, 2, 3, 4].map((idx) => (
                       <button
                         key={idx}
