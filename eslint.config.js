ab653cae1c4e0acb6c218eb1f0673eb45f78dcb6
ignite-education <hello@ignite.education>
2025-12-31 14:18:12 +0000

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'check-lesson-data.js', 'backfill_stripe_ids.js']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
