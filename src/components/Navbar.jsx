7368d42d12c92d2727ad69d0da0c561433ef6799
ignite-education <hello@ignite.education>
2026-01-22 21:39:07 +0000

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
 
   return (
     <div className="sticky top-0 z-50 bg-black">
-      <div className="p-5 flex items-center justify-between">
+      <div className="px-10 py-5 flex items-center justify-between">
         {/* Logo - links to home */}
         <Link to="/" className="inline-block">
           <div
