b240fcf24bb8e4936f3cd9dd829e7d9531a241c8
ignite-education <hello@ignite.education>
2026-02-16 19:51:05 +0000

    Update EducationSection.tsx

--- next-app/src/app/welcome/EducationSection.tsx
+++ next-app/src/app/welcome/EducationSection.tsx
@@ -112,7 +112,7 @@ export default function EducationSection() {
         >
           <div
             className={`grid text-center auth-promises-list ${isMobile ? 'grid-cols-1' : 'grid-cols-3'}`}
-            style={{ width: '100%', gap: '2rem' }}
+            style={{ width: '100%', gap: '0.5rem' }}
           >
             {showFeatures && (
               <>
