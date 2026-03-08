70c22588c0ae8af6b2aa2fc48471e64f4db42597
ignite-education <hello@ignite.education>
2026-03-08 21:00:23 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1528,7 +1528,7 @@ app.post('/api/create-checkout-session', async (req, res) => {
       payment_method_types: ['card'],
       line_items: [
         {
-          price: 'price_1T8lhxRxlg2WD2fji8waWyQD',
+          price: 'price_1T8oOgRxlg2WD2fjpPae6DNZ',
           quantity: 1,
         },
       ],
